8cbfc8976c79f3c2ea5701a3cead340301ccb3b4
Rune Moskvil Lyngås <runely85@gmail.com>
2020-06-29 14:27:40 +0200

    Moved mail validation and sending out to support queue trigger as well

--- SendMail/index.js
+++ SendMail/index.js
@@ -1,45 +1,7 @@
-const validateInput = require('../lib/validate-input');
-const generateMail = require('../lib/generate-mail');
-const sendMail = require('../lib/send-mail');
+const handleMail = require('../lib/handle-mail');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const { validationMatched, validationError, validationMessage } = await validateInput(req);
-    if (validationMatched) {
-        let mail = generateMail(req.body);
-        let { mailStatus, mailError } = await sendMail(mail);
-        if (mailStatus) {
-            context.res = {
-                body: mail,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            };
-        }
-        else {
-            context.res = {
-                status: 500,
-                body: {
-                    error: mailError || '',
-                    mail
-                },
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            };
-        }
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: {
-                message: validationMessage,
-                error: validationError || ''
-            },
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-    }
+    context.res = await handleMail(req.body);
 }
